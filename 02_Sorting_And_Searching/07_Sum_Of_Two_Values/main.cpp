fc49f69a4ac70a466742328569c0ec9934146253
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-02 10:08:40 -0500

    Add Sum Of Two Values

---
+++ 02_Sorting_And_Searching/07_Sum_Of_Two_Values/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    map<int, int> ma;
+    int n, x; cin >> n >> x;
+    for (int i = 1; i <= n; i++) {
+        int v; cin >> v;
+        if (ma.find(x - v) != ma.end()) {
+            cout << ma[x - v] << ' ' << i << '\n';
+            return;
+        }
+        ma[v] = i;
+    }
+    cout << "IMPOSSIBLE\n";
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
