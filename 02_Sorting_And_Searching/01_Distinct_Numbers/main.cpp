5578a061ed3e4f8ca3ed168d70204faefff0f5cf
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-02 09:56:12 -0500

    Add Distinct Numbers

---
+++ 02_Sorting_And_Searching/01_Distinct_Numbers/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    set<int> se;
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        se.insert(x);
+    }
+    cout << se.size() << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
