602cc19e11ecc720de4d1f43e00fe8865aa6bb63
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-02 10:09:25 -0500

    Add Maximum Subarray Sum

---
+++ 02_Sorting_And_Searching/08_Maximum_Subarray_Sum/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    int n; cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    ll ans = a[0], sum = a[0];
+    for (int i = 1; i < n; i++) {
+        sum = max(a[i], sum + a[i]);
+        ans = max(ans, sum);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
