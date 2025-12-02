35628cec2671e15b4e035fa6f5a3b7ae9d2e9fe5
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-02 10:23:14 -0500

    Add Playlist

---
+++ 02_Sorting_And_Searching/13_Playlist/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    map<int, int> ma;
+    int n; cin >> n;
+    int last = -1, ans = 1;
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        if (ma.find(k) != ma.end()) last = max(last, ma[k]);
+        ans = max(ans, i - last);
+        ma[k] = i;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
