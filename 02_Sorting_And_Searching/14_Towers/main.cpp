2c8f636cafa2740e341b25c62cd1a7c960814cc4
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-02 10:33:04 -0500

    Add Towers

---
+++ 02_Sorting_And_Searching/14_Towers/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    int n; cin >> n;
+    multiset<int> se;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        auto it = se.upper_bound(x);
+        if (it != se.end()) se.erase(it);
+        se.insert(x);
+    }
+    cout << se.size() << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
