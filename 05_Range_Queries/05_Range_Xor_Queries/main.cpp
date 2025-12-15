9e06731dd7e57a5eeff622ec2840ae3dab7c4fae
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-15 14:49:07 -0500

    Add Range Xor Queries

---
+++ 05_Range_Queries/05_Range_Xor_Queries/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    int n, q; cin >> n >> q;
+    vector<int> arr(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        arr[i] ^= arr[i - 1];
+    }
+    while (q--) {
+        int a, b; cin >> a >> b;
+        cout << (arr[a - 1] ^ arr[b]) << '\n';
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
