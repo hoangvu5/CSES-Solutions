fa473fbe54992396482fe641d50918aef70ec34f
hoangvu5 <minhhoangvu0505@gmail.com>
2025-12-15 14:24:16 -0500

    Add Static Range Minimum Queries

---
+++ 05_Range_Queries/02_Static_Range_Minimum_Queries/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+template<class T>
+struct RMQ {
+    vector<vector<T>> jmp;
+    RMQ(const vector<T>& V) : jmp(1, V) {
+        for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
+            jmp.emplace_back(sz(V) - pw * 2 + 1);
+            rep(j,0,sz(jmp[k]))
+                jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
+        }
+    }
+    T query(int a, int b) {
+        assert(a < b); // or return inf if a == b
+        int dep = 31 - __builtin_clz(b - a);
+        return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+    }
+};
+
+void solve() {
+    int n, q; cin >> n >> q;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    RMQ<int> rmq(arr);
+    while (q--) {
+        int a, b; cin >> a >> b;
+        cout << rmq.query(--a, b) << '\n';
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
