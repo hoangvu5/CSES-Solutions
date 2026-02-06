5e1c6447ad1979a32152d81090ce11ddb0625c65
hoangvu5 <minhhoangvu0505@gmail.com>
2026-02-06 13:50:50 -0500

    Add Binomial Coefficient

---
+++ 07_Mathematics/11_Binomial_Coefficient/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int M = 1e9 + 7;
+const int m = 1e6 + 1;
+int fact[m], inv[m];
+
+int expo(int a, int b) {
+    int ans = 1, cur = a;
+    while (b) {
+        if (b & 1) ans = 1ll * ans * cur % M;
+        cur = 1ll * cur * cur % M;
+        b /= 2;
+    }
+    return ans;
+}
+
+int C(int a, int b) {
+    return 1ll * fact[a] * inv[b] % M * inv[a - b] % M;
+}
+
+void solve() {
+    fact[0] = 1;
+    for (int i = 1; i < m; i++) {
+        fact[i] = 1ll * fact[i-1] * i % M;
+    }
+
+    inv[m-1] = expo(fact[m-1], M - 2);
+    for (int i = m - 2; i >= 0; i--) {
+        inv[i] = 1ll * inv[i+1] * (i + 1) % M;
+    }
+
+    int n; cin >> n;
+    int a, b;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << C(a, b) << '\n';
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
