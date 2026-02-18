c29cd0b0bb364045949416c44599dfc1d1c1261b
hoangvu5 <minhhoangvu0505@gmail.com>
2026-02-18 13:39:25 -0500

    Add Stair Game

---
+++ 07_Mathematics/35_Stair_Game/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    int n; cin >> n;
+    int sum = 0, x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (i % 2 == 1) sum ^= x;
+    }
+    cout << (sum == 0 ? "second" : "first") << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin >> t;
+    while (t--) solve();
+}
