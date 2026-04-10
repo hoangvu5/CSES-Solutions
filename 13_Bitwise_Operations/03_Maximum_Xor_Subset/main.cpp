e8bba18b53a297952ef271c2800d41ab69498442
hoangvu5 <minhhoangvu0505@gmail.com>
2026-04-10 11:03:03 -0400

    Add Maximum Xor Subset

---
+++ 13_Bitwise_Operations/03_Maximum_Xor_Subset/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cout<<#x<<" = "<<x<<'\n'
+#define dbgv(x) for(int i=0;i<x.size();i++)cout<<x[i]<<' ';cout<<'\n'
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    // Max XOR sum of a subset
+    // Find basis for the set
+    // Construct maximum XOR
+    int n; cin >> n;
+    int rank = 0;
+    vector<ll> basis(31);
+    for (int i = 0; i < n; i++) {
+        ll x; cin >> x;
+        for (int j = 30; j >= 0; j--) {
+            if (!((x >> j) & 1)) continue;
+            if (!basis[j]) { basis[j] = x; rank++; break; }
+            x ^= basis[j];
+        }
+    }
+    for (int j = 30; j >= 0; j--) {
+        if (!basis[j]) continue;
+        for (int k = j - 1; k >= 0; k--) {
+            if ((basis[j] >> k) & 1) basis[j] ^= basis[k];
+        }
+    }
+    ll ans = 0;
+    for (int j = 30; j >= 0; j--) {
+        ans = max(ans, ans ^ basis[j]);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
